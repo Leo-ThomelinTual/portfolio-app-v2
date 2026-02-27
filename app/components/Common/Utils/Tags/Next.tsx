8f4a1c975a8d18d51fe50388cdf2b31fd9148434
Léo <leo.thomelintual@gmail.com>
2026-02-27 14:05:34 +0100

    -

--- app/components/Common/Utils/Tags/Next.tsx
+++ app/components/Common/Utils/Tags/Next.tsx
@@ -1,15 +1,7 @@
 import Tags from "./Tags";
 
-type NextProps = {
-  onClick?: unknown;
-};
-
 const Next = () => {
-  return (
-    <Tags id="next" dataTag="next" icon="devicon:nextjs">
-      NextJS
-    </Tags>
-  );
+  return <Tags icon="devicon:nextjs">NextJS</Tags>;
 };
 
 export default Next;
--- app/components/Common/Utils/Tags/Tags.tsx
+++ app/components/Common/Utils/Tags/Tags.tsx
@@ -3,16 +3,12 @@ import { Icon } from "@iconify/react";
 
 type TagsProps = {
   icon: string;
-  id: string;
-  dataTag: string;
   children: React.ReactNode;
 };
 
-const Tags = ({ icon, children, id, dataTag }: TagsProps) => {
+const Tags = ({ icon, children }: TagsProps) => {
   return (
     <button
-      id={id}
-      data-tag={dataTag}
       aria-labelledby={`Tags : ${children}`}
       className="transition-border ease flex h-10 w-max items-center gap-2 rounded-md border-2 border-(--color-border-muted) bg-(--color-background) px-2 text-center duration-300 select-none hover:border-(--color-border)"
     >
